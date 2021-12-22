4a5525518d085e5ea1ce71953054a6af0a1aeee7
jonaspleyer <jonas.pleyer@fdm.uni-freiburg.de>
2021-12-22 16:31:40 +0100

    added comments and variables

--- start.sh
+++ start.sh
@@ -1,9 +1,19 @@
-rm ./secretion_project
+ODIR_VALIDITY="../Validity_Testing/PDE-Single-Point-Source/output"
+PROJECT_NAME="./secretion_project"
+BASE_DIR=$(pwd)
+
+# Clean old files and logs
+rm $PROJECT_NAME
 rm output/logs*
+
+# Build new program files
 cd build
 make
 cd ..
-./secretion_project
+
+# Run PhysiCell
+$PROJECT_NAME
+
 #cd build
 #make movie
 #cd ..
