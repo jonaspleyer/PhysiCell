c44f612326e81fd5648945adbea9156a8c16c145
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-04-29 09:55:00 +0200

    now plot for 2 different substrates and save in respective files.

--- plot_controller.py
+++ plot_controller.py
@@ -11,25 +11,27 @@ def get_acceptable_data(values, buffer=5, sigma=0.0001):
     return acc
 
 if __name__ == "__main__":
-    filename = "controller_logs.txt"
-    names = np.loadtxt(filename, dtype=str, delimiter=",", max_rows=1)
-    results = np.loadtxt(filename, delimiter=",", skiprows=1)
+    for l in range(2):
+        filename = "controller_logs_" + str(l+1) + ".txt"
+        names = np.loadtxt(filename, dtype=str, delimiter=",", max_rows=1)
+        results = np.loadtxt(filename, delimiter=",", skiprows=1)
 
-    # Create figure with correct size and 4 plot regions
-    f = plt.figure(figsize=(12, 8))
-    f, axes = plt.subplots(4, 1, sharex=True)
-    axes[0].set_title("Controller Metrics")
-    for i, name in enumerate(names[1:-2]):
-        acc = get_acceptable_data(results[:,i+1])
-        axes[i].plot(results[:,0][acc], results[:,i+1][acc], label=name, color="k")
-        axes[i].legend()
-        # axes[-1].set_ylabel("Controller Module Responses")
-        axes[i].tick_params(axis="x",direction="in")
-    
-    acc = get_acceptable_data(results[:,-1])
-    axes[-1].plot(results[:,0][acc], results[:,-1][acc], label="Difference $\Delta=V_t-V_o$", color="k")
-    axes[-1].legend()
-    axes[-1].set_xlabel("Update steps [10 min]")
-    f.tight_layout()
-    f.subplots_adjust(hspace=0)
-    f.savefig("results.png")
+        # Create figure with correct size and 4 plot regions
+        f = plt.figure(figsize=(12, 8))
+        f, axes = plt.subplots(4, 1, sharex=True)
+        axes[0].set_title("Controller Metrics")
+        for i, name in enumerate(names[1:-2]):
+            acc = get_acceptable_data(results[:,i+1])
+            axes[i].plot(results[:,0][acc], results[:,i+1][acc], label=name, color="k")
+            axes[i].legend()
+            # axes[-1].set_ylabel("Controller Module Responses")
+            axes[i].tick_params(axis="x",direction="in")
+        
+        acc = get_acceptable_data(results[:,-1])
+        axes[-1].plot(results[:,0][acc], results[:,-1][acc], label="Difference $\Delta=V_t-V_o$", color="k")
+        axes[-1].legend()
+        axes[-1].set_xlabel("Update steps [10 min]")
+        f.tight_layout()
+        f.subplots_adjust(hspace=0)
+        f.savefig("results_" + str(l+1) + ".png")
+        plt.clf()
