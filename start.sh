545f4a9c35e55d97a6728b44a92684cb011b8958
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-11-30 11:47:02 +0100

    optimizations for really big runs (such that workstation does not crash)

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 PROJECT_NAME="./project"
 BASE_DIR=$(pwd)
-THREADS=44
+THREADS=30
 
 # Clean old files and logs
 rm $PROJECT_NAME
@@ -29,14 +29,22 @@ fi
 if (($ret == 0));
 then
 	# convert from svg to png in parallel
-	parallel -j $THREADS convert -size 842x901 {} {.}.png ::: $(ls output/snapshot*.svg)
+	for i in $(seq 0 9); do parallel -j $THREADS convert -size 842x901 {} {.}.png ::: $(ls output/snapshot00000$i*.svg); done
 
 	# Create movie
 	# ffmpeg -y -threads 40 -r 15 -f image2 -pattern_type glob -i 'output/*.png' -vcodec libx264 -pix_fmt yuv444p -strict -2 -tune animation movie.mp4
 	# ffmpeg -y -pattern_type glob -i 'output/*.png' -c:v libx264 -b:v 15000k -minrate 5000k -maxrate 8000k -pass 1 -pix_fmt yuv444p -an -f mp4 /dev/null && \
 	# ffmpeg -pattern_type glob -i 'output/*.png' -c:v libx264 -b:v 15000k -minrate 5000k -maxrate 8000k -pass 2 -pix_fmt yuv444p -c:a aac -b:a 192k -movflags faststart output.mp4
 	ffmpeg -y -pattern_type glob -i 'output/*.png' -c:v libx264 -b:v 15000k -minrate 5000k -maxrate 15000k -pass 1 -pix_fmt yuv422p -an -f mp4 /dev/null && \
-	ffmpeg -pattern_type glob -i 'output/*.png' -c:v libx264 -b:v 15000k -minrate 20000k -maxrate 30000k -pass 2 -pix_fmt yuv422p -c:a aac -b:a 192k -movflags faststart output.mp4
+	ffmpeg -y -pattern_type glob -i 'output/*.png' -c:v libx264 -b:v 15000k -minrate 20000k -maxrate 30000k -pass 2 -pix_fmt yuv422p -c:a aac -b:a 192k -movflags faststart output.mp4
 else
 	echo "Error in Execution of $PROJECT_NAME: Do not continue"
 fi
+
+if (($ret == 0));
+then
+	python3 quickplot.py
+else
+	echo "Erorr in Creation of Video. Do not plot."
+fi
+
