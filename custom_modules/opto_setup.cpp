caa5ed76c1f9b844d9d94d88780f5d0a5ec22be9
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-11-30 11:43:30 +0100

    create new controllers for managing cell density and simply dumping information to single csv file

--- custom_modules/opto_setup.cpp
+++ custom_modules/opto_setup.cpp
@@ -39,100 +39,27 @@ void run_optogenetics ( const double& t ) {
 
 // *********************************************************************************
 // DENSITY CONTROLLER MODULES
-Val DensityObservableSphere::measure(Kernel::Sphere_3& _domain, std::vector<PhysiCell::Cell*> cells) {
+Val DensityObservableCuboid::measure(Kernel::Iso_cuboid_3& _domain, std::vector<PhysiCell::Cell*> cells) {
     int N_cells = std::count_if(cells.begin(), cells.end(), [](PhysiCell::Cell* cell){return !cell->phenotype.death.dead;});
-    std::cout << "Cells living in domain: " << N_cells << std::endl;
+
     // double domain_size = 4.0*M_PI*std::pow(_domain.squared_radius(),3/2)/3.0;
     // Write this to a file for plots later
     std::ofstream outfile;
     outfile.open("information.csv", std::ios_base::app);
-    outfile << N_cells << "," << cells.size() << "," << PhysiCell::parameters.doubles("cell_target_sphere") << "\n";
+    outfile << id_x << "," << id_y << "," << N_cells << "," << cells.size() << "," << PhysiCell::parameters.doubles("cell_target_boxes") << "\n";
 
     return N_cells;
 }
 
 
 double DensityMetric::calculate(Val& target, Val& observed) {
-    return fmax(0.0, (observed-target)/target);
+    return observed-target;
 }
 
 
 double DensityControllFunctor::adjust(std::deque<double> state) {
     // Target is implicitly alays 0.0
     return state.back();
-}
-
-
-void DensityEffect::apply(PhysiCell::Cell* cell, const double discrepancy) {
-    // Determine threshold with which cells will die
-    // Parameter a controls how steep the curve will rise for the threshold
-    double a = 0.5;
-    double thresh = fmin(fmax(0.0, 1/(1+a*discrepancy)), 1.0);
-    double rand = PhysiCell::UniformRandom();
-    // std::cout << "Discrepancy: " << discrepancy << " threshold: " << thresh << " random: " << rand << std::endl;
-    if (rand > thresh ) {
-        cell->start_death(cell->phenotype.death.find_death_model_index(100));
-    }
-    return;
-}
-
-
-// *********************************************************************************
-// DIFFERENTIATION CONTROLLER MODULES
-Val Diff_1_ObservableCuboid::measure(Kernel::Iso_cuboid_3& _domain, std::vector<PhysiCell::Cell*> cells) {
-    int N_cells = std::count_if(
-        cells.begin(),
-        cells.end(),
-        [this](PhysiCell::Cell* cell){
-            // Check that cell is not dead
-            bool cell_not_dead = !cell->phenotype.death.dead;
-            // Check if cells are differentiated
-            bool cell_correct_type = fabs(cell->custom_data["diff"] - diff_enable_1) < 0.1;
-            return cell_not_dead && cell_correct_type;
-        }
-        );
-    // std::cout << "matching cells in domain: " << N_cells << std::endl;
-    return N_cells;
-}
-
-
-Val Diff_2_ObservableSphere::measure(Kernel::Sphere_3& _domain, std::vector<PhysiCell::Cell*> cells) {
-    int N_cells = std::count_if(
-        cells.begin(),
-        cells.end(),
-        [this](PhysiCell::Cell* cell){
-            // Check that cell is not dead
-            bool cell_not_dead = !cell->phenotype.death.dead;
-            // Check if cells are differentiated
-            bool cell_correct_type = fabs(cell->custom_data["diff"] - diff_enable_2) < 0.1;
-            return cell_not_dead && cell_correct_type;
-        }
-        );
-    std::cout << "matching cells in domain: " << N_cells << std::endl;
-
-    return N_cells;
-}
-
-
-Val Diff_3_ObservableSphere::measure(Kernel::Sphere_3& _domain, std::vector<PhysiCell::Cell*> cells) {
-    int N_cells = std::count_if(
-        cells.begin(),
-        cells.end(),
-        [this](PhysiCell::Cell* cell){
-            // Check that cell is not dead
-            bool cell_not_dead = !cell->phenotype.death.dead;
-            // Check if cells are differentiated
-            bool cell_correct_type = fabs(cell->custom_data["diff"] - diff_enable_3) < 0.1;
-            return cell_not_dead && cell_correct_type;
-        }
-        );
-    // std::cout << "matching cells in domain: " << N_cells << std::endl;
-    return N_cells;
-}
-
-
-double Diff_Metric::calculate(Val& target, Val& observed) {
-    return (observed-target)/target;
 }
 
 
@@ -144,101 +71,21 @@ double PD_Controllfunctor::adjust(std::deque<double> state) {
 }
 
 
-void Diff_1_Effect::apply(PhysiCell::Cell* cell, const double discrepancy) {
-    // Check if cells meets criteria
-    bool cell_diff_disable = fabs(cell->custom_data["diff"] - diff_disable) < 0.1;
-    bool cell_in_diff_1 = fabs(cell->custom_data["diff"] - diff_enable_1) < 0.1;
-    bool cell_not_dead = !cell->phenotype.death.dead;
+void DensityEffect::apply(PhysiCell::Cell* cell, const double discrepancy) {
+    double dt = PhysiCell::parameters.doubles("optogenetics_dt");
 
-    // Get a random parameter between 0 and 1
-    double rand = PhysiCell::UniformRandom();
-    // Determine threshhold via discrepancy
-    double thresh = 1/fabs(discrepancy);
-    
-    // Check that the cell is not already dead
-    if (cell_not_dead) {
-        if (discrepancy > 0.0) {
-            // If discrepancy is positive (more differentiated cells than targeted) kill some cells
-            if (rand > thresh && cell_in_diff_1) {
-                cell->start_death(cell->phenotype.death.find_death_model_index(100));
-            }
-        } else if (
-            // Check if the cell is not yet differentiated
-            cell_diff_disable && rand > 1-fabs(discrepancy)/(1+fabs(discrepancy))
-        ) {
-            // If the cell is not differentiated yet, the probability
-            // to differentiate is given by the discrepancy
-            cell->custom_data["diff"] = diff_enable_1;
-            cell->phenotype.motility.migration_speed = new_migr_speed;
-            cell->phenotype.mechanics.cell_cell_adhesion_strength = new_adh_strength;
-        }
-    }
+    double increase = std::min(
+        PhysiCell::parameters.doubles("light_ion_exposure_increment") * discrepancy,
+        PhysiCell::parameters.doubles("light_ion_exposure_increment_max")
+    );
+
+    // Increase the ion cocnentration by the amount of light exposure which is given
+	cell->custom_data["light_ion_concentration"] += dt * increase;
     return;
 }
 
 
-void Diff_2_Effect::apply(PhysiCell::Cell* cell, const double discrepancy) {
-    // Check if cells meets criteria
-    bool cell_diff_disable = fabs(cell->custom_data["diff"] - diff_disable) < 0.1;
-    bool cell_in_diff_2 = fabs(cell->custom_data["diff"] - diff_enable_2) < 0.1;
-    bool cell_not_dead = !cell->phenotype.death.dead;
-
-    // Get a random parameter between 0 and 1
-    double rand = PhysiCell::UniformRandom();
-    // Determine threshhold via discrepancy
-    double thresh = 1/fabs(discrepancy);
-    
-    // Check that the cell is not already dead
-    if (cell_not_dead) {
-        if (discrepancy > 0.0) {
-            // If discrepancy is positive (more differentiated cells than targeted) kill some cells
-            if (rand > thresh && cell_in_diff_2) {
-                cell->start_death(cell->phenotype.death.find_death_model_index(100));
-            }
-        } else if (
-            // Check if the cell is not yet differentiated
-            cell_diff_disable && rand > 1-fabs(discrepancy)/(1+fabs(discrepancy))
-        ) {
-            // If the cell is not differentiated yet, the probability
-            // to differentiate is given by the discrepancy
-            cell->custom_data["diff"] = diff_enable_2;
-            cell->phenotype.volume.multiply_by_ratio(volume_multiplier);
-        }
-    }
-    return;
-}
-
-
-void Diff_3_Effect::apply(PhysiCell::Cell* cell, const double discrepancy) {
-    // Check if cells meets criteria
-    bool cell_diff_disable = fabs(cell->custom_data["diff"] - diff_disable) < 0.1;
-    bool cell_in_diff_3 = fabs(cell->custom_data["diff"] - diff_enable_3) < 0.1;
-    bool cell_not_dead = !cell->phenotype.death.dead;
-
-    // Get a random parameter between 0 and 1
-    double rand = PhysiCell::UniformRandom();
-    // Determine threshhold via discrepancy
-    double thresh = 1/fabs(discrepancy);
-    
-    // Check that the cell is not already dead
-    if (cell_not_dead) {
-        if (discrepancy > 0.0) {
-            // If discrepancy is positive (more differentiated cells than targeted) kill some cells
-            if (rand > thresh && cell_in_diff_3) {
-                cell->start_death(cell->phenotype.death.find_death_model_index(100));
-            }
-        } else if (
-            // Check if the cell is not yet differentiated
-            cell_diff_disable && rand > 1-fabs(discrepancy)/(1+fabs(discrepancy))
-        ) {
-            // If the cell is not differentiated yet, the probability
-            // to differentiate is given by the discrepancy
-            cell->custom_data["diff"] = diff_enable_3;
-            cell->phenotype.motility.migration_speed = diff_3_speed;
-        }
-    }
-    return;
-}
+void LoggingEffect::apply(PhysiCell::Cell* cell, const double discrepancy) { return; }
 
 
 // *********************************************************************************
@@ -253,92 +100,63 @@ void setup_optogenetics( void ) {
     std::cout << "          Setting up OptoGen          " << std::endl;
     std::cout << "**************************************\n" << std::endl;
 
-    std::cout << PhysiCell::parameters.doubles("density_circle_radius") << "\n";
-    auto sphere_outer = Kernel::Sphere_3(
-        Kernel::Point_3(0, 0, 0),
-        pow(PhysiCell::parameters.doubles("density_circle_radius"), 2.0));
 
-    auto cont = new DensityController(
-        sphere_outer,
-        PhysiCell::parameters.doubles("cell_target_sphere")
-    );
-    supervisor.add_controller("Density_controller", cont);
+    // Create boxes with individual controllers
+    int n_boxes = PhysiCell::parameters.ints("n_boxes_along_axis");
+
+    // Get the size of the total simulation domain
+    double Xmin = microenvironment.mesh.bounding_box[0];
+	double Ymin = microenvironment.mesh.bounding_box[1];
+	double Zmin = microenvironment.mesh.bounding_box[2];
+
+	double Xmax = microenvironment.mesh.bounding_box[3];
+	double Ymax = microenvironment.mesh.bounding_box[4];
+	double Zmax = microenvironment.mesh.bounding_box[5];
+
+    double dX = (Xmax-Xmin)/n_boxes/2.0;
+    double dY = (Ymax-Ymin)/n_boxes/2.0;
+    
+    for (int i=0; i<2*n_boxes; i++) {
+        for (int j=0; j<2*n_boxes; j++) {
+            // Define the box
+            auto cuboid = Kernel::Iso_cuboid_3(
+                Kernel::Point_3(Xmin + dX * i,      Ymin + dY * j,      Zmin),
+                Kernel::Point_3(Xmin + dX * i + dX, Ymin + dY * j + dY, Zmax)
+            );
+            
+            // If we are at an even vertical position, we take the even boxes horizontally
+            if ((i+j) % 2==1) {
+                // Define the controller for this box
+                auto cont = new DensityController(
+                    cuboid,
+                    PhysiCell::parameters.doubles("cell_target_boxes"),
+                    i,
+                    j
+                );
+
+                // Controller is named by location on grid
+                std::string name = "Density_controller_" + std::to_string(i) + "_" + std::to_string(j);
+
+                // Add the controller to the supervisor such that is automatically run
+                supervisor.add_controller(name, cont);
+            }
+            // Otherwise we simply use a dummy controller for logging purposes
+            else {
+                // Define the controller for this box
+                auto cont = new LoggingController(cuboid, 0, i, j);
+
+                // Controller is named by location on grid
+                std::string name = "Logging_controller_" + std::to_string(i) + "_" + std::to_string(j);
+
+                // Add the controller to the supervisor such that is automatically run
+                supervisor.add_controller(name, cont);
+            }
+        }
+    }
 
     std::ofstream outfile;
     outfile.open("information.csv");
-    outfile << "N_cells,total cells,setpoint\n";
-
-    /*std::vector<Kernel::Iso_cuboid_3> diff_1_domains = {
-        // Left hand side
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc-sm*xt*3/2, yc-sm*yt*3/2, zc-sm*zt/2), Kernel::Point_3( xc-sm*xt*1/2, yc-sm*yt*1/2, zc+sm*zt/2)),
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc-sm*xt*3/2, yc-sm*yt*1/2, zc-sm*zt/2), Kernel::Point_3( xc-sm*xt*1/2, yc+sm*yt*1/2, zc+sm*zt/2)),
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc-sm*xt*3/2, yc+sm*yt*1/2, zc-sm*zt/2), Kernel::Point_3( xc-sm*xt*1/2, yc+sm*yt*3/2, zc+sm*zt/2)),
-        // Right hand side
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc+sm*xt*1/2, yc-sm*yt*3/2, zc-sm*zt/2), Kernel::Point_3( xc+sm*xt*3/2, yc-sm*yt*1/2, zc+sm*zt/2)),
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc+sm*xt*1/2, yc-sm*yt*1/2, zc-sm*zt/2), Kernel::Point_3( xc+sm*xt*3/2, yc+sm*yt*1/2, zc+sm*zt/2)),
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc+sm*xt*1/2, yc+sm*yt*1/2, zc-sm*zt/2), Kernel::Point_3( xc+sm*xt*3/2, yc+sm*yt*3/2, zc+sm*zt/2)),
-        // Middle
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc-sm*xt*1/2, yc-sm*yt*3/2, zc-sm*zt/2), Kernel::Point_3( xc+sm*xt*1/2, yc-sm*yt*1/2, zc+sm*zt/2)),
-        Kernel::Iso_cuboid_3(Kernel::Point_3(xc-sm*xt*1/2, yc+sm*yt*1/2, zc-sm*zt/2), Kernel::Point_3( xc+sm*xt*1/2, yc+sm*yt*3/2, zc+sm*zt/2))
-    };
-
-    std::vector<Kernel::Sphere_3> diff_2_domains = {
-        Kernel::Sphere_3(Kernel::Point_3(xc, yc, zc), pow(PhysiCell::parameters.doubles("diff_2_circle_radius"), 2.0))
-    };
-
-    std::vector<Kernel::Sphere_3> diff_3_domains = {
-        // Bottom left
-        Kernel::Sphere_3(Kernel::Point_3(BioFVM::microenvironment.mesh.bounding_box[0], BioFVM::microenvironment.mesh.bounding_box[1], zc), pow(PhysiCell::parameters.doubles("diff_3_circle_radius"), 2.0)),
-        // Bottom right
-        Kernel::Sphere_3(Kernel::Point_3(BioFVM::microenvironment.mesh.bounding_box[0], BioFVM::microenvironment.mesh.bounding_box[4], zc), pow(PhysiCell::parameters.doubles("diff_3_circle_radius"), 2.0)),
-        // Top left
-        Kernel::Sphere_3(Kernel::Point_3(BioFVM::microenvironment.mesh.bounding_box[3], BioFVM::microenvironment.mesh.bounding_box[1], zc), pow(PhysiCell::parameters.doubles("diff_3_circle_radius"), 2.0)),
-        // Top right
-        Kernel::Sphere_3(Kernel::Point_3(BioFVM::microenvironment.mesh.bounding_box[3], BioFVM::microenvironment.mesh.bounding_box[4], zc), pow(PhysiCell::parameters.doubles("diff_3_circle_radius"), 2.0))
-    };
-
-    int i = 0;
-    for (auto const& _domain : diff_1_domains) {
-        auto cont = new Diff_1_Controller(
-            // Observable and Effect Domain
-            // Kernel::Iso_cuboid_3(Kernel::Point_3(-150.0, -150.0, -10.0), Kernel::Point_3(150.0, 150.0, 10.0)),
-            _domain,
-            // Target: How many differentiated cells do we want to have?
-            PhysiCell::parameters.doubles("Target_cells_per_domain_diff_1")
-        );
-        supervisor.add_controller("Diff_Controller_diff_1_" + std::to_string(i), cont);
-        i++;
-    }*/
-
-    // Same for diff_2 domains
-    /*i=0;
-    for (auto const& _domain : diff_2_domains) {
-        auto cont = new Diff_2_Controller(
-            _domain,
-            PhysiCell::parameters.doubles("Target_cells_per_domain_diff_2")
-        );
-        supervisor.add_controller("Diff_Controller_diff_2_" + std::to_string(i), cont);
-        i++;
-    }*/
-
-    // Same for diff_3 domains
-    /*i=0;
-    for (auto const& _domain : diff_3_domains) {
-        auto cont = new Diff_3_Controller(
-            _domain,
-            PhysiCell::parameters.doubles("Target_cells_per_domain_diff_3")
-        );
-        supervisor.add_controller("Diff_Controller_diff_3_" + std::to_string(i), cont);
-        i++;
-    }*/
-
-    // TODO CURRENTLY NOT WORKING
-    // Add visitor to save state to csv file
-    // supervisor.add_visitor_update("Safe_state", new Visitor_Write_State_to_csv("testfile.csv"));
-    // supervisor.update_visit_all_controllers("Safe_state");
-
-    // auto cb1 = Kernel::Iso_cuboid_3(Kernel::Point_3(-300.0, -300.0, -10.0), Kernel::Point_3(300.0, 300.0, 10.0));
-    // auto cb2 = Kernel::Iso_cuboid_3(Kernel::Point_3(-100.0, 0.0, -10.0), Kernel::Point_3(250.0, 500.0, 10.0));
+    outfile << "id_x,id_y,N_cells,total cells,setpoint\n";
 
     std::cout << "**************************************\n" << std::endl;
 }
--- custom_modules/opto_setup.h
+++ custom_modules/opto_setup.h
@@ -29,9 +29,23 @@ class DensityControllFunctor : public Opto::Controller::ControllFunctor {
 };
 
 
-class DensityObservableSphere : public Opto::Controller::Observable<Val, Kernel::Sphere_3> {
+class PD_Controllfunctor : public Opto::Controller::ControllFunctor {
 	public:
-		Val measure(Kernel::Sphere_3& _domain, std::vector<PhysiCell::Cell*> cells) override;
+		// Proportional multiplication constant
+		double K_p = 5.0;
+		// Differential multiplication constant
+		double K_d = 5.0;
+		// Time constant between update steps
+		double update_dt = PhysiCell::parameters.doubles("optogenetics_update_dt");
+		double adjust(std::deque<double> state);
+};
+
+
+class DensityObservableCuboid : public Opto::Controller::Observable<Val, Kernel::Iso_cuboid_3> {
+	public:
+		Val measure(Kernel::Iso_cuboid_3& _domain, std::vector<PhysiCell::Cell*> cells) override;
+		int id_x;
+		int id_y;
 };
 
 
@@ -41,171 +55,69 @@ class DensityMetric : public Opto::Controller::Metric<Val> {
 };
 
 
-class DensityEffect : public Opto::Controller::Effect<Kernel::Sphere_3> {
+class DensityEffect : public Opto::Controller::Effect<Kernel::Iso_cuboid_3> {
     public:
 		std::unique_ptr<Opto::Light::LightSource> lightsource = std::make_unique<Red_LED>();
+		double current_light_intensity = 0.0;
         void apply(PhysiCell::Cell* cell, const double discrepancy);
 };
 
 
-class DensityController : public Opto::Controller::Controller<Val, DensityController, Kernel::Sphere_3, Kernel::Sphere_3> {
+class DensityController : public Opto::Controller::Controller<Val, DensityController, Kernel::Iso_cuboid_3, Kernel::Iso_cuboid_3> {
 public:
 	DensityController(
-		Kernel::Sphere_3 _sphere,
-		Val _target
+		Kernel::Iso_cuboid_3 _domain,
+		Val _target,
+		int id_x,
+		int id_y
 	) {
-		observable->observable_domain = _sphere;
-		effect->effect_domain = _sphere;
+		observable->observable_domain = _domain;
+		effect->effect_domain = _domain;
 		target = _target;
+
+		observable->id_x = id_x;
+		observable->id_y = id_y;
 	}
 
-	Kernel::Sphere_3 domain;
-	std::unique_ptr<DensityObservableSphere> observable = std::make_unique<DensityObservableSphere>();
+	Kernel::Iso_cuboid_3 domain;
+	std::unique_ptr<DensityObservableCuboid> observable = std::make_unique<DensityObservableCuboid>();
     Val target{};
     std::unique_ptr<DensityMetric> metric = std::make_unique<DensityMetric>();
-    std::unique_ptr<DensityControllFunctor> controllfunctor = std::make_unique<DensityControllFunctor>();
+    std::unique_ptr<PD_Controllfunctor> controllfunctor = std::make_unique<PD_Controllfunctor>();
     std::unique_ptr<DensityEffect> effect = std::make_unique<DensityEffect>();
 };
 
 
-// *********************************************************************************
-// DIFFERENTIATION CONTROLLER MODULES
-class PD_Controllfunctor : public Opto::Controller::ControllFunctor {
-	public:
-		// Proportional multiplication constant
-		double K_p = 0.001;
-		// Differential multiplication constant
-		double K_d = 0.001;
-		// Time constant between update steps
-		double update_dt = PhysiCell::parameters.doubles("optogenetics_update_dt");
-		double adjust(std::deque<double> state);
-};
-
-
-class Diff_1_ObservableCuboid : public Opto::Controller::Observable<Val, Kernel::Iso_cuboid_3> {
-	public:
-		double diff_disable = PhysiCell::parameters.doubles("diff_disable");
-		double diff_enable_1 = PhysiCell::parameters.doubles("diff_enable_1");
-		Kernel::Iso_cuboid_3 domain;
-		Val measure(Kernel::Iso_cuboid_3& _domain, std::vector<PhysiCell::Cell*> cells) override;
-};
-
-
-class Diff_2_ObservableSphere : public Opto::Controller::Observable<Val, Kernel::Sphere_3> {
-	public:
-		double diff_disable = PhysiCell::parameters.doubles("diff_disable");
-		double diff_enable_2 = PhysiCell::parameters.doubles("diff_enable_2");
-		Kernel::Sphere_3 domain;
-		Val measure(Kernel::Sphere_3& _domain, std::vector<PhysiCell::Cell*> cells) override;
-};
-
-
-class Diff_3_ObservableSphere : public Opto::Controller::Observable<Val, Kernel::Sphere_3> {
-	public:
-		double diff_disable = PhysiCell::parameters.doubles("diff_disable");
-		double diff_enable_3 = PhysiCell::parameters.doubles("diff_enable_3");
-		Kernel::Sphere_3 domain;
-		Val measure(Kernel::Sphere_3& _domain, std::vector<PhysiCell::Cell*> cells) override;
-};
-
-
-class Diff_Metric : public Opto::Controller::Metric<Val> {
-	public:
-		double calculate(Val& v1, Val& v2);
-};
-
-
-class Diff_1_Effect : public Opto::Controller::Effect<Kernel::Iso_cuboid_3> {
+class LoggingEffect : public Opto::Controller::Effect<Kernel::Iso_cuboid_3> {
     public:
-		double new_migr_speed = PhysiCell::parameters.doubles("diff_1_migration_speed");
-		double new_adh_strength = PhysiCell::parameters.doubles("diff_1_adhesion_strength");
-		
-		double diff_disable = PhysiCell::parameters.doubles("diff_disable");
-		double diff_enable_1 = PhysiCell::parameters.doubles("diff_enable_1");
 		std::unique_ptr<Opto::Light::LightSource> lightsource = std::make_unique<Red_LED>();
+		double current_light_intensity = 0.0;
         void apply(PhysiCell::Cell* cell, const double discrepancy);
 };
 
 
-class Diff_2_Effect : public Opto::Controller::Effect<Kernel::Sphere_3> {
-    public:
-		double volume_multiplier = PhysiCell::parameters.doubles("diff_2_volume_multiplier");
-		
-		double diff_disable = PhysiCell::parameters.doubles("diff_disable");
-		double diff_enable_2 = PhysiCell::parameters.doubles("diff_enable_2");
-		std::unique_ptr<Opto::Light::LightSource> lightsource = std::make_unique<Red_LED>();
-        void apply(PhysiCell::Cell* cell, const double discrepancy);
-};
-
-
-class Diff_3_Effect : public Opto::Controller::Effect<Kernel::Sphere_3> {
-    public:
-		double diff_3_speed = PhysiCell::parameters.doubles("diff_3_speed");
-
-		double diff_disable = PhysiCell::parameters.doubles("diff_disable");
-		double diff_enable_3 = PhysiCell::parameters.doubles("diff_enable_3");
-		std::unique_ptr<Opto::Light::LightSource> lightsource = std::make_unique<Red_LED>();
-        void apply(PhysiCell::Cell* cell, const double discrepancy);
-};
-
-
-class Diff_1_Controller : public Opto::Controller::Controller<Val, Diff_1_Controller, Kernel::Iso_cuboid_3, Kernel::Iso_cuboid_3> {
+class LoggingController : public Opto::Controller::Controller<Val, LoggingController, Kernel::Iso_cuboid_3, Kernel::Iso_cuboid_3> {
 public:
-	Diff_1_Controller(
-		Kernel::Iso_cuboid_3 _cuboid,
-		Val _target
+	LoggingController(
+		Kernel::Iso_cuboid_3 _domain,
+		Val _target,
+		int id_x,
+		int id_y
 	) {
-		observable->observable_domain = _cuboid;
-		effect->effect_domain = _cuboid;
+		observable->observable_domain = _domain;
+		effect->effect_domain = _domain;
 		target = _target;
+
+		observable->id_x = id_x;
+		observable->id_y = id_y;
 	}
 
 	Kernel::Iso_cuboid_3 domain;
-	std::unique_ptr<Diff_1_ObservableCuboid> observable = std::make_unique<Diff_1_ObservableCuboid>();
+	std::unique_ptr<DensityObservableCuboid> observable = std::make_unique<DensityObservableCuboid>();
     Val target{};
-    std::unique_ptr<Diff_Metric> metric = std::make_unique<Diff_Metric>();
+    std::unique_ptr<DensityMetric> metric = std::make_unique<DensityMetric>();
     std::unique_ptr<PD_Controllfunctor> controllfunctor = std::make_unique<PD_Controllfunctor>();
-    std::unique_ptr<Diff_1_Effect> effect = std::make_unique<Diff_1_Effect>();
-};
-
-
-class Diff_2_Controller : public Opto::Controller::Controller<Val, Diff_2_Controller, Kernel::Sphere_3, Kernel::Sphere_3> {
-public:
-	Diff_2_Controller(
-		Kernel::Sphere_3 _sphere,
-		Val _target
-	) {
-		observable->observable_domain = _sphere;
-		effect->effect_domain = _sphere;
-		target = _target;
-	}
-
-	Kernel::Sphere_3 domain;
-	std::unique_ptr<Diff_2_ObservableSphere> observable = std::make_unique<Diff_2_ObservableSphere>();
-    Val target{};
-    std::unique_ptr<Diff_Metric> metric = std::make_unique<Diff_Metric>();
-    std::unique_ptr<PD_Controllfunctor> controllfunctor = std::make_unique<PD_Controllfunctor>();
-    std::unique_ptr<Diff_2_Effect> effect = std::make_unique<Diff_2_Effect>();
-};
-
-
-class Diff_3_Controller : public Opto::Controller::Controller<Val, Diff_3_Controller, Kernel::Sphere_3, Kernel::Sphere_3> {
-public:
-	Diff_3_Controller(
-		Kernel::Sphere_3 _sphere,
-		Val _target
-	) {
-		observable->observable_domain = _sphere;
-		effect->effect_domain = _sphere;
-		target = _target;
-	}
-
-	Kernel::Sphere_3 domain;
-	std::unique_ptr<Diff_3_ObservableSphere> observable = std::make_unique<Diff_3_ObservableSphere>();
-    Val target{};
-    std::unique_ptr<Diff_Metric> metric = std::make_unique<Diff_Metric>();
-    std::unique_ptr<PD_Controllfunctor> controllfunctor = std::make_unique<PD_Controllfunctor>();
-    std::unique_ptr<Diff_3_Effect> effect = std::make_unique<Diff_3_Effect>();
+    std::unique_ptr<LoggingEffect> effect = std::make_unique<LoggingEffect>();
 };
 
 
